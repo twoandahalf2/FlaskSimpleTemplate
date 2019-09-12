fc27fec150f63ac543c99932b11fd97580064be6
twoandahalf2 <vladimir.kolev@hotmail.com>
2019-09-12 09:48:21 +0300

    Initial commit

---
+++ flaskblog.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for
+
+###to run FLASK WEB SERVER:
+#execute command to register as env variables. second one refreshes the server live
+#export FLASK_APP=flaskblog.py
+#export FLASK_DEBUG=1
+#flask run
+## add name == main and run the file directly =  works better.
+
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
